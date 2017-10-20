039c92e96af5d65b8a0b074bf5be997d0423cc54
Aron Wouters <lionar@mail.com>
2017-10-20 14:43:47 +0200

    Added ability to set console app name.

--- source/app.php
+++ source/app.php
@@ -11,10 +11,12 @@ use Illuminate\Events\Dispatcher as events;
 
 class app extends console
 {
-	public function __construct ( container $container, dispatcher $dispatcher = null, float $version = 1 )
+	public function __construct ( container $container, string $name = 'Firestark', 
+		dispatcher $dispatcher = null, float $version = 1 )
 	{
 		$dispatcher = ( $dispatcher ) ?: new events;
 		parent::__construct ( $container, $dispatcher, $version );
+		$this->setName ( $name );
 	}
 
 	public function command ( string $signature, closure $command )
