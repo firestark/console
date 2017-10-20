4994a2d02c8d62d4ee62b79f515783634420ce2e
Aron Wouters <lionar@mail.com>
2017-10-20 13:51:29 +0200

    Making dispatcher and version optional.

--- source/app.php
+++ source/app.php
@@ -4,10 +4,19 @@ namespace console;
 
 use closure;
 use Illuminate\Console\Application as console;
+use Illuminate\Contracts\Container\Container as container;
+use Illuminate\Contracts\Events\Dispatcher as dispatcher;
+use Illuminate\Events\Dispatcher as events;
 
 
 class app extends console
 {
+	public function __construct ( container $container, dispatcher $dispatcher = null, float $version = 1 )
+	{
+		$dispatcher = ( $dispatcher ) ?: new events;
+		parent::__construct ( $container, $dispatcher, $version );
+	}
+
 	public function command ( string $signature, closure $command )
 	{
 		$command = new closureCommand ( $signature, $command );
